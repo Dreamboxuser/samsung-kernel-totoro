491d8a53905078e5e41513e076d0f1b82e5e8076
PsychoGame <jajadekroon@gmail.com>
2013-01-25 12:55:08 +0100

    Based on GT-S5360_GB_Opensource_Update3, added new defconfig bcm21553_totoro_defconfig in which ext2 and ext3 modules are replaced with ext4 module which is backwards compatible.

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Thu Jan 24 11:43:02 CET 2013"
+#define LINUX_COMPILE_TIME "11:43:02"
+#define LINUX_COMPILE_BY "jasper"
+#define LINUX_COMPILE_HOST "jasper-300E4A-300E5A-300E7A"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
